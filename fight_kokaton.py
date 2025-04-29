7f528bb181132e7c8624ac0053ef90d9ca4802fc
c0a24017 <c0a24017@edu.teu.ac.jp>
2025-04-29 17:45:10 +0900

    修正済み #1

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -149,7 +149,8 @@ class Score:
     def __init__(self):
         self.fonto = pg.font.SysFont("hgp創英角ﾎﾟｯﾌﾟ体",30)
         self.score = 0
-        self.img = self.fonto.render(f"スコア:{self.score}", 0, (0,0,255))
+        self.color = (0, 0, 255)
+        self.img = self.fonto.render(f"スコア:{self.score}", 0, self.color)
         self.rct = self.img.get_rect()
         self.rct.bottomleft = (100, HEIGHT - 50)
 
